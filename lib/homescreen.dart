5e29c51e6427b580d2eaaeea0428bf6b0037a90d
lenovo <eman>
2022-04-01 17:17:24 +0200

    intial empty homepage

---
+++ lib/homescreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class homescreen extends StatelessWidget {
+  static const String routename = 'home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
